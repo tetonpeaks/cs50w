01deccdca0eba79907902deeda98f393f8485c79
shodson02 <98664424+shodson02@users.noreply.github.com>
2024-04-19 16:11:53 -0400

    commit00

--- mail/views.py
+++ mail/views.py
@@ -106,7 +106,6 @@ def mailbox(request, mailbox):
 @login_required
 def email(request, email_id):
 
-    # Query for requested email
     try:
         email = Email.objects.get(user=request.user, pk=email_id)
     except Email.DoesNotExist:
